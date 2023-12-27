a3a6f5dc8288cbb25ddded3c6323c7dd2f3ea60f
shannoj <69207315+shannoj@users.noreply.github.com>
2023-12-27 12:48:51 +0100

    fixed bug where an extra underscore was added to the end of the seraut object

--- Script.R
+++ Script.R
@@ -2,18 +2,19 @@ dirs <- list.dirs(path = '~/Desktop/Bioinfromatics/integration_data/', recursive
 for (x in dirs){
  name <- gsub('filtered_feature_bc_matrix', "", x)
   
- cts <- ReadMtx(mtx = paste0("~/Desktop/Bioinfromatics/integration_data/", x, "/matrix.mtx.gz"),
-          features = paste0("~/Desktop/Bioinfromatics/integration_data/", x, "/features.tsv.gz"),
-          cells = paste0("~/Desktop/Bioinfromatics/integration_data/", x, "/barcodes.tsv.gz"))
+ cts <- ReadMtx(mtx = paste0("~/Desktop/Bioinfromatics/integration_data/",x, "/matrix.mtx.gz"),
+          features = paste0("~/Desktop/Bioinfromatics/integration_data/",x, "/features.tsv.gz"),
+          cells = paste0("~/Desktop/Bioinfromatics/integration_data/",x, "/barcodes.tsv.gz"))
+ name <- sub("_+$", "", name)
  assign(name, CreateSeuratObject(counts = cts))
 }
 
-merged_seraut <- merge(HB17_background_, y = c(HB17_PDX_, HB30_tumor_, HB17_tumor_, HB53_tumor_, HB53_background_, HB30_PDX_), add.cell.ids = ls()[3:9], project = 'HB')
+merged_seraut <- merge(HB17_background, y = c(HB17_PDX, HB30_tumor, HB17_tumor, HB53_tumor, HB53_background, HB30_PDX), add.cell.ids = ls()[3:9], project = 'HB')
 
 merged_seraut$sample <- rownames(merged_seraut@meta.data)
 
 merged_seraut@meta.data <- separate(merged_seraut@meta.data, col = 'sample', into = c("Patient", "Type", "Barcode"), sep = '_')
-view(merged_seraut)
 
 view(merged_seraut)
+
 
