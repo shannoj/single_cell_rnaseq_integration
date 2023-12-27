7c11333a7ac50cd5e7aaddee40894e8f51e60a84
shannoj <69207315+shannoj@users.noreply.github.com>
2023-12-27 12:31:12 +0100

    seperated columns in the merged seraut object

--- Script.R
+++ Script.R
@@ -10,3 +10,10 @@ for (x in dirs){
 
 merged_seraut <- merge(HB17_background_, y = c(HB17_PDX_, HB30_tumor_, HB17_tumor_, HB53_tumor_, HB53_background_, HB30_PDX_), add.cell.ids = ls()[3:9], project = 'HB')
 
+merged_seraut$sample <- rownames(merged_seraut@meta.data)
+
+merged_seraut@meta.data <- separate(merged_seraut@meta.data, col = 'sample', into = c("Patient", "Type", "Barcode"), sep = '_')
+view(merged_seraut)
+
+view(merged_seraut)
+
